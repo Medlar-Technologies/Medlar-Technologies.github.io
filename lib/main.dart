c9508fb0cdbec83ba11242490ce0878949e0e4cd
Medlar-Technologies <medlartechnologiesllc@gmail.com>
2023-02-12 14:56:23 -0500

    test 2

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'flutter test change',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'flutter test change'),
     );
   }
 }
